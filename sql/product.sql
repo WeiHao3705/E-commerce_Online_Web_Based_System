194daeb823565422226acd9026e13515de795fe0
liewcb <143771761+liewcb@users.noreply.github.com>
2025-11-16 17:38:02 +0800

    resolve table problem

--- sql/product.sql
+++ sql/product.sql
@@ -2,9 +2,7 @@ CREATE TABLE product (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
-    description TEXT,
-    main_image_id INT NULL,
-    FOREIGN KEY (main_image_id) REFERENCES product_image(id)
+    description TEXT
 );
 
 -- test
