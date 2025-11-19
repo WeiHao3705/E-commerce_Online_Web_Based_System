0809baeffa7b18acb649f7ede84d91d99406596a
Hhehhe37 <142575377+Hhehhe37@users.noreply.github.com>
2025-11-19 13:04:35 +0800

    Update user.sql

--- sql/user.sql
+++ sql/user.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     gender ENUM('Male','Female','Other') DEFAULT 'Other',
     contact_no VARCHAR(20),
     email VARCHAR(100) NOT NULL UNIQUE,
-    profile_photo MEDIUMLOB,
+    profile_photo MEDIUMBLOB,
     password VARCHAR(255) NOT NULL,
     security_question VARCHAR(255),
     security_answer VARCHAR(255),
